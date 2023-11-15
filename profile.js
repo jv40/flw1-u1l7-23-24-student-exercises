5735fac8f8cb2402839952d15ea42f774a31e360
jv40 <139980633+jv40@users.noreply.github.com>
2023-11-15 23:25:39 +0000

    Done

--- profile.js
+++ profile.js
@@ -7,10 +7,10 @@ let main = document.querySelector(".main");
 button.addEventListener("click", function() {
 
     // 1. Updated the innerHTML of profileName to your name or a made up one.
-
+  profileName.innerHTML = "Bananyas";
     // 2. Updated the src of profileImage a picture of you or one that makes sense.
-
+profileImage.src = "https://static.wikia.nocookie.net/bananya/images/8/8c/Bananya.pn";
     // 3. Updated the src of main to a photo that's be your first post.
-
+main.src="https://thicc-af.mywaifulist.moe/waifus/momonga-overlord/1Y3mRHUwkOulZxKTumAKj0AQk9BNrNPfAq8K5IoZ.jpg";
 
 });
--- stringInterpolation.js
+++ stringInterpolation.js
@@ -2,15 +2,16 @@
 let greeting = document.querySelector(".greeting");
 
 // 1. Create a variable called `firstName` and set the value to your name.
-
+let firstName="Jonathan";
 
 // 2. Create a variable called `lastName` and set the value to your last name.
-
+let lastName="Vasquez";
 
 
 // 3. Create a variable called `greetingMessage`.
 // - Using string interpolation, create a messga that say "Hi, (your first name) (your last name)!"
-
+let greetingMessage=`Hi ${firstName + ` `+ lastName}`;
 
 
 // 4. Set the innerHTML of the greeting div to the greeting message.
+greeting.innerHTML= greetingMessage;
